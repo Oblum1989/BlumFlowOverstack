42062d45554dde1c0326efdfbb9cbae6c46747b4
Oblum1989 <oscar.blum@gmail.com>
2019-07-06 21:29:52 -0500

    deploy heroku 1.1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  #config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -73,7 +74,7 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
-  config.assets.compile = true
+
 
   
 
